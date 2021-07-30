235120da24a560f1a7f53ac4ad6c3fb566300211
Nahuel Castro <nahukas@gmail.com>
2021-07-30 23:27:39 +0200

    Finish initial design

---
+++ src/components/NewProject.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewProject: React.FC = () => (
+  <button className="mt-2 -ml-1 flex items-center text-sm font-medium text-gray-600">
+    <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24">
+      <path
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        d="M12 7v10m5-5H7"
+      />
+    </svg>
+    <span className="ml-1">New Project</span>
+  </button>
+);
+
+export default NewProject;
