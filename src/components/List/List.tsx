3e4b7c13d216ed37dcaeb57a2031765c1e9a3593
Nahuel Castro <nahukas@gmail.com>
2021-07-31 10:54:39 +0200

    Finish card component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { ReactChildren, ReactChild } from 'react';
 import Title from './Title';
 
-const List: React.FC = () => {
+interface ListProps {
+  children: ReactChild | ReactChild[] | ReactChildren | ReactChildren[];
+}
+
+const List = ({ children }: ListProps) => {
   return (
     <section className="ml-3 flex-shrink-0 p-3 w-80 bg-gray-100 rounded-sm">
       <Title />
+      <ul className="mt-2">{children}</ul>
     </section>
   );
 };
